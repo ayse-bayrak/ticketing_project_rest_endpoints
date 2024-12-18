8bb3dd2cfb97e320ac78ed95d9cb7f3b705e4a14
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-12-18 09:21:16 +0300

    ticketing project is separated some part, this part is related only endpoints

---
+++ src/test/java/com/cydeo/ticketing_project_rest_endpoints/TicketingProjectRestEndpointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.ticketing_project_rest_endpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingProjectRestEndpointsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
